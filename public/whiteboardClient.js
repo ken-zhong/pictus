f91760258c56fd6accca73ad4593968b927f7675
Kennan Zhong <kenzhong@Kennans-Air.fios-router.home>
2017-07-21 18:49:45 -0400

    refactored code

--- public/whiteboardClient.js
+++ public/whiteboardClient.js
@@ -2,13 +2,10 @@
 /* global io */
 /* global $ */
 
+const socket = io();
+const boardId = window.location.pathname.slice(8);
 var canvas = new fabric.Canvas("draw");
-var socket = io();
-var boardId = window.location.pathname.slice(8);
 
-// this implementation of setting global false and turning select true on objects is 
-// glitching with our socket loading; each refresh resets the true/false of object selection
-//fabric.Object.prototype.selectable = false;
 
 var app = {
     init: function(){
@@ -17,31 +14,26 @@ var app = {
       canvas.set("isDrawingMode", "true");
       canvas.freeDrawingBrush.width = 1;
     },
-    
+
     initSockets: function(){
       socket.on('connect', function(){
         socket.emit('room', boardId);
       });
-      
+
       socket.on('boardState', function(val){
-        updateCanvas(val);
-      });
-      
-      canvas.on('path:created', function(){
-        sendCanvas();
-      });
-      
-      canvas.on('object:modified', function(){
-        sendCanvas();
+        app.updateCanvas(val);
       });
 
-      // this listener interferes with our clearPaths function; moved sendCanvas to object removal function
-      // canvas.on('object:removed', function(){
-      //   sendCanvas();
-      // });
+      canvas.on('path:created', function(){
+        app.sendCanvas();
+      });
+
+      canvas.on('object:modified', function(){
+        app.sendCanvas();
+      });
     },
-      
-    initListeners: function(){  
+
+    initListeners: function(){
         $(document).on('keydown', function(event){
           // if user holds down shift key, they can select / move objects around on the canvas
           if(event.which === 16){
@@ -51,181 +43,182 @@ var app = {
           }
           //let users delete objects that they have currently selected with backspace or del key
           if(event.which === 8 || event.which === 46){
-            var activeObject = canvas.getActiveObject();
+            let activeObject = canvas.getActiveObject();
             canvas.remove(activeObject);
-            sendCanvas();
+            app.sendCanvas();
           }
         });
-        
+
         $(document).on('keyup', function(event){
           if(event.which === 16){
             canvas.isDrawingMode = true;
           }
         });
-        
+
         $('#tokenCreate').on('click', function(){
-          var name = $('#tokenName').val();
-          var color = $('#tokenColor').val();
-          createToken(name, color);
+          let name = $('#tokenName').val();
+          let color = $('#tokenColor').val();
+          whiteboard.createToken(name, color);
           $('#tokenName').val('');
-          sendCanvas();
+          app.sendCanvas();
         });
 
         $('#textCreate').on('click', function(){
-          var text = $('#newText').val();
-          var size = $('input[name=textSizeRadio]:checked').val();
-          createText(text, size);
+          let text = $('#newText').val();
+          let size = $('input[name=textSizeRadio]:checked').val();
+          whiteboard.createText(text, size);
           $('#newText').val('');
-          sendCanvas();
+          app.sendCanvas();
         });
 
         $('.color-btn').each(function(){
           $(this).on('click', function(){
-            var color = $(this).val();
-            changeBrushColor(color);
+            let color = $(this).val();
+            whiteboard.changeBrushColor(color);
           });
         });
-        
+
         $('#brush-eraser-btn').on('click', function(){
-          changeToEraser();
+          whiteboard.changeToEraser();
         });
-        
+
         $('#draw-mode-btn').on('click', function(){
-          drawMode();
+          whiteboard.drawMode();
         });
 
         $('#reset-canvas-btn').on('click', function(){
-          resetCanvas();
-        });        
+          app.resetCanvas();
+        });
 
-        $('#clear-paths-btn').on('click', function(){
-          clearPaths();
-        });        
+        // $('#clear-paths-btn').on('click', function(){
+        //   clearPaths();
+        // });
+    },
+
+    sendCanvas: function(){
+      let newData = {};
+      newData.JSON = app.saveCanvas();
+      newData.room = boardId;
+      socket.emit('update', newData);
+    },
+
+    updateCanvas: function(newCanvas){
+      //pass refreshCanvas in so that it runs after the load;
+      //was giving me errors where it would refresh before the new canvas loads
+      canvas.loadFromJSON(newCanvas, app.refreshCanvas);
+    },
+
+    refreshCanvas: function(){
+      // this check makes sure that anything drawn is not selectable,
+      // and objects are brought to the top so that you can't erase objects with the eraser
+      canvas._objects.forEach(function(element){
+        if(element.type === "path"){
+          element.selectable = false;
+        } else {
+          canvas.bringToFront(element);
+        }
+      });
+    },
+
+    resetCanvas: function(){
+      canvas.loadFromJSON('{"objects":[]}');
+      this.sendCanvas();
+    },
+
+    saveCanvas: function(){
+      return JSON.stringify(canvas);
     }
 };
 
-function sendCanvas(){
-  var newData = {};
-  newData.JSON = saveCanvas();
-  newData.room = boardId;
-  socket.emit('update', newData); 
-}
+var whiteboard = {
+  drawMode: function(){
+    canvas.isDrawingMode = !canvas.isDrawingMode;
+    app.refreshCanvas();
+  },
 
-function updateCanvas(newCanvas){
-  //pass refreshCanvas in so that it runs after the load; 
-  //was giving me errors where it would refresh before the new canvas loads
-  canvas.loadFromJSON(newCanvas, refreshCanvas); 
-}
-
-function refreshCanvas(){
-  // this check makes sure that anything drawn is not selectable, 
-  // and objects are brought to the top so that you can't erase objects with the eraser
-  canvas._objects.forEach(function(element){
-    if(element.type === "path"){
-      element.selectable = false;
-    } else {
-      canvas.bringToFront(element);
+  changeBrushColor: function(color){
+    if(!canvas.isDrawingMode){
+      canvas.isDrawingMode = true;
     }
-  });
-}
+    canvas.freeDrawingBrush.color = color;
+    canvas.freeDrawingBrush.width = 1;
+  },
 
-function resetCanvas(){
-  canvas.loadFromJSON('{"objects":[]}');
-  sendCanvas();
-}
-
-function saveCanvas(){
-  return JSON.stringify(canvas);
-}
-
-function drawMode(){
-  canvas.isDrawingMode = !canvas.isDrawingMode;
-  refreshCanvas();
-}
-
-function changeBrushColor(color){
-  if(!canvas.isDrawingMode){
-    canvas.isDrawingMode = true;
-  }
-  canvas.freeDrawingBrush.color = color;
-  canvas.freeDrawingBrush.width = 1;
-}
-
-function changeToEraser(){
-  if(!canvas.isDrawingMode){ 
-    canvas.isDrawingMode = true;
-  }
-  canvas.freeDrawingBrush.color = 'white';
-  canvas.freeDrawingBrush.width = 90;
-}
-
-function createToken(name ="A", color="black"){
-  if(name.length > 2){
-    name = name[0] + name[1];
-  } else if(name.length === 0){
-    name = 'A';
-  } 
-  
-  var coordinates = randomCoordinates(500, 500);
-  
-  var circle = new fabric.Circle({
-    radius: 20,
-    fill: 'white',
-    originX: 'center',
-    originY: 'center',
-    strokeWidth: 2,
-    stroke: color
-  });
-
-  var text = new fabric.Text(name, {
-    fontSize: 16,
-    originX: 'center',
-    originY: 'center',
-    fill: color,
-    fontWeight: 'bold',
-    fontFamily: 'arial'
-  });
-
-  var token = new fabric.Group([circle, text], {
-    left: coordinates.x,
-    top: coordinates.y,
-    selectable: true
-  });
-
-  canvas.add(token);
-}
-
-function randomCoordinates(xLimit, yLimit){
-  var x = Math.floor(Math.random()*xLimit);
-  var y = Math.floor(Math.random()*yLimit);
-  return {x: x, y: y};
-}
-
-function clearPaths(){
-  // **BUG** why doesn't this work properly? Only removes some paths with each call. Async issue I think
-  canvas._objects.forEach(function(elem){
-    if(elem.type === "path"){
-      canvas.remove(elem);
+  changeToEraser: function(){
+    if(!canvas.isDrawingMode){
+      canvas.isDrawingMode = true;
     }
-  });
-  sendCanvas();
-}
+    canvas.freeDrawingBrush.color = 'white';
+    canvas.freeDrawingBrush.width = 90;
+  },
 
-function createText(newText, size = 16){
-  var text = new fabric.Text(newText, {
-    fontSize: size,
-    left: 150,
-    top: 150
-  });
-  canvas.add(text);
-}
+  createToken: function(name, color="black"){
+    if(name.length > 2){
+      name = name[0] + name[1];
+    } else if(name.length === 0){
+      name = 'A';
+    }
 
-function createImage(url){
-  fabric.Image.fromURL(url, function(newImage){
-    canvas.add(newImage);
-    sendCanvas();
-  });
-  refreshCanvas();
-}
+    let coordinates = this.randomCoordinates(500, 500);
+
+    let circle = new fabric.Circle({
+      radius: 20,
+      fill: 'white',
+      originX: 'center',
+      originY: 'center',
+      strokeWidth: 2,
+      stroke: color
+    });
+
+    let text = new fabric.Text(name, {
+      fontSize: 16,
+      originX: 'center',
+      originY: 'center',
+      fill: color,
+      fontWeight: 'bold',
+      fontFamily: 'arial'
+    });
+
+    let token = new fabric.Group([circle, text], {
+      left: coordinates.x,
+      top: coordinates.y,
+      selectable: true
+    });
+
+    canvas.add(token);
+  },
+
+  randomCoordinates: function(xLimit, yLimit){
+    let x = Math.floor(Math.random()*xLimit);
+    let y = Math.floor(Math.random()*yLimit);
+    return {x: x, y: y};
+  },
+
+  createText: function(newText, size = 16){
+    let text = new fabric.Text(newText, {
+      fontSize: size,
+      left: 150,
+      top: 150
+    });
+    canvas.add(text);
+  },
+
+  createImage: function(url){
+    fabric.Image.fromURL(url, function(newImage){
+      canvas.add(newImage);
+      app.sendCanvas();
+    });
+    app.refreshCanvas();
+  },
+
+  clearPaths: function(){
+    canvas._objects.forEach(function(elem){
+      if(elem.type === "path"){
+        canvas.remove(elem);
+      }
+    });
+    app.sendCanvas();
+  }
+};
 
 app.init();
